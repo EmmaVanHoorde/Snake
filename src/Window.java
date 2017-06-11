01e79580a61956f3d0b05ff95ec352303e6915fd
Unknown <vanhoorde.em@gmail.com>
2017-06-12 01:16:34 +0200

    window

--- src/Window.java
+++ src/Window.java
@@ -15,17 +15,33 @@ public class Window extends JFrame{
         Start start = new Start();
 
         this.setTitle("Snake");
-        this.setSize(600, 400);
+        this.setSize(600, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
 
         JButton buttonPlay = new JButton("niveau 1");
         JButton buttonPlay2 = new JButton("niveau 2");
         JButton buttonLeave = new JButton("Quitter");
+
         buttonPlay.setPreferredSize(new Dimension(100, 30));
         buttonPlay2.setPreferredSize(new Dimension(100, 30));
         buttonLeave.setPreferredSize(new Dimension(100, 30));
 
+        buttonPlay.setForeground(new Color(255, 255, 255));
+        buttonPlay.setBackground(new Color(220, 20, 60));
+        buttonPlay.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonPlay.setBorder(null);
+
+        buttonPlay2.setForeground(new Color(255, 255, 255));
+        buttonPlay2.setBackground(new Color(220, 20, 60));
+        buttonPlay2.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonPlay2.setBorder(null);
+
+        buttonLeave.setForeground(new Color(255, 255, 255));
+        buttonLeave.setBackground(new Color(220, 20, 60));
+        buttonLeave.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonLeave.setBorder(null);
+        
         this.setLayout(new FlowLayout());
         this.add(buttonPlay);
         this.add(buttonPlay2);
